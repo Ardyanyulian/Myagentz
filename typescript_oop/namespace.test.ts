f7b50d19580570e760db5a66e1a16a71b04c3204
Ardyanyulian <muhamadardyan23@gmail.com>
2025-01-24 22:33:31 +0700

    3.1th commit deleted some file

--- typescript_oop/namespace.test.ts
+++
@@ -1,7 +0,0 @@
-import { mathutilis } from "../dist/sc/math-util";
-describe('namespace', () => {
-    it('should be return to masth utilis', ()  => {
-        console.info(mathutilis.PI)
-    })
-
-})
